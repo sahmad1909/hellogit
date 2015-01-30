c36aa5bd8a801b7a4d77641b337ec56c128f847d
sahmad1909 <sikandarahmad@hotmail.com>
2015-01-29 21:12:22 -0800

    Initial commit

---
+++ helloworld/src/main/java/com/calstatela/cs454/helloworld/App.java
@@ -0,0 +1,13 @@
+package com.calstatela.cs454.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
